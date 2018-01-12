e8d84c317f6897742423ddf835ef9d19b768d2b2
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-12 12:16:27 -0800

    Completes branching functionality for all tracks and includes styling to hide form and fadeIn the track suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,42 @@
+var percentFit = function (trackScore, trackPossibleScore) {
+	var trackPercent = ((trackScore / trackPossibleScore) * 100);
+	return trackPercent
+}
+
+
+
 $(document).ready(function() {
   $("form#trackSurvey").submit(function(event) {
+		$("form#trackSurvey").hide();
+		$(".trackOption").hide();
 		var name = $("#name").val();
 		var email = $("#email").val();
 		var education = $("#education").val();
 		var comfort = $("#computerComfort").val();
 		var codeInterest = $("#codeInterest").val();
 		var companySize = $("#companySize").val();
-		$("#rubyTrack").show();
-		$("#phpTrack").show();
-		$("#javaTrack").show();
-		$("#cssTrack").show();
-		$("#csharpTrack").show();
+
+		if (comfort === "uncomfortable" || comfort === "lowComfort") {
+			$("#introTrack").fadeIn();
+		}	else if (codeInterest === "intMobile") {
+			$("#javaTrack").fadeIn();
+		}	else if (codeInterest === "intDesign") {
+			$("#cssTrack").fadeIn();
+		}	else if (codeInterest === "intBackend") {
+			$("#phpTrack").fadeIn();
+		}	else if (codeInterest === "intBtoB") {
+			$("#csharpTrack").fadeIn();
+		} else if (companySize === "smStartup" || companySize === "mdStartup" || codeInterest === "intInteractive") {
+			$("#rubyTrack").fadeIn();
+		} else {
+			alert("Sorry! We're not exactly sure which track you should take.")
+		}
+
+		// $("#rubyTrack").show();
+		// $("#phpTrack").show();
+		// $("#javaTrack").show();
+		// $("#cssTrack").show();
+		// $("#csharpTrack").show();
 		event.preventDefault();
 	});
 });
